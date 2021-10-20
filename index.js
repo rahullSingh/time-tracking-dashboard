6bc6e5b3b9b0cdb9c50494ff685bb60f03bd6341
rahul kumar s <rahu143rspt3gailcom>
2021-10-20 07:01:39 +0530

    user-timeline

---
+++ index.js
@@ -0,0 +1,89 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+  // alert("event")
+  anime({
+    targets: '.thirtTwo',
+    innerHTML: [0, 32],
+    easing: 'linear',
+    round: 1
+  });
+
+  anime({
+    targets: '.thirtSix',
+    innerHTML: [0, 36],
+    easing: 'linear',
+    round: 1
+  });
+
+  anime({
+    targets: '.four',
+    innerHTML: [0, 4],
+    easing: 'linear',
+    round: 1
+  });
+
+  anime({
+    targets: '.five',
+    innerHTML: [0, 5],
+    easing: 'linear',
+    round: 1
+  });
+
+  anime({
+    targets: '.ten',
+    innerHTML: [0, 10],
+    easing: 'linear',
+    round: 1
+  });
+
+  anime({
+    targets: '.nine',
+    innerHTML: [0, 9],
+    easing: 'linear',
+    round: 1
+  });
+
+  // anime({
+  //   targets: '.five',
+  //   innerHTML: [0, 32],
+  //   easing: 'linear',
+  //   round: 1
+  // });
+
+  anime({
+    targets: '.seven',
+    innerHTML: [0, 7],
+    easing: 'linear',
+    round: 1
+  });
+
+  // anime({
+  //   targets: '.4',
+  //   innerHTML: [0, 4],
+  //   easing: 'linear',
+  //   round: 1
+  // });
+
+  // anime({
+  //   targets: '.5',
+  //   innerHTML: [0, 32],
+  //   easing: 'linear',
+  //   round: 1
+  // });
+
+  // anime({
+  //   targets: '.5',
+  //   innerHTML: [0, 32],
+  //   easing: 'linear',
+  //   round: 1
+  // });
+
+  anime({
+    targets: '.six',
+    innerHTML: [0, 6],
+    easing: 'linear',
+    round: 1
+  });
+
+})
